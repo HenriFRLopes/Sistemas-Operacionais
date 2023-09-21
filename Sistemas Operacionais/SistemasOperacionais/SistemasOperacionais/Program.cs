bca508a367e02da32f375737afe47ba9484e3450
Henri Lopes <henrilopes16@gmail.com>
2023-09-21 08:45:32 -0300

    Menu e funções Place Holder
    Adicionei o menu do banco de dados e funções vazias para o que a gente vai precisar pra primeira entrega

---
+++ Sistemas Operacionais/SistemasOperacionais/SistemasOperacionais/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SistemasOperacionais
+{
+    class Program
+    {
+        string path = "bancoDeDados.db";
+        static void Main(string[] args)
+        {
+            string key = Console.ReadLine();
+            string value = Console.ReadLine();
+            string action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "Search":
+                    string found = Search(key);
+                    if (found != null) Console.WriteLine(found);
+                    else Console.WriteLine("Key does not exist");
+                    break;
+                case "Insert":
+                    if (Insert(key, value)) Console.WriteLine("Successful insertion");
+                    else Console.WriteLine("Key already Inserted");
+                    break;
+                case "Update":
+                    if (Update(key, value)) Console.WriteLine("Successfully Updated");
+                    else Console.WriteLine("Key does not exist");
+                    break;
+                case "Remove":
+                    if (Remove(key)) Console.WriteLine("Successfully removed");
+                    else Console.WriteLine("Key does not exist");
+                    break;
+            }
+        }
+        static string Search(string key)
+        {
+
+        }
+        static bool Insert(string key, string v)
+        {
+
+        }
+        static bool Update(string key, string v)
+        {
+        }
+        static bool Remove(string key)
+        {
+
+        }
+    }
+}
