5ac16bff0bd0c91ecdddcc345f08d6e323032ffb
LucasB07 <lucb0056@gmail.com>
2023-09-24 20:42:06 -0300

    documentação e revisão do codigo
    corrigindo alguns erros e adicionando comentários

--- Sistemas Operacionais/SistemasOperacionais/SistemasOperacionais/Program.cs
+++ Sistemas Operacionais/SistemasOperacionais/SistemasOperacionais/Program.cs
@@ -5,18 +5,24 @@ namespace SistemasOperacionais
 {
     class Program
     {
-        string path = "bancoDeDados.db";
-        string temporaryFile = "Temporary_";
+        //variaveis do arquivo de banco de dados
+        const string path = "bancoDeDados.db";
+        const string temporaryFile = "Temporary_";
         static void Main(string[] args)
         {
+            //pega as linhas de comando e divide em variaveis sepados
             if (args.Length == 0) return;
             string[] split = args[0].Split('=');
             string[] keyAndValue = split[1].Split(':');
+            //cria uma variavel do tipo Data
             Data d = new Data(keyAndValue[0], keyAndValue[1]);
             string action = split[0];
 
             switch (action)
             {
+                default:
+                    Console.WriteLine("Invalid Action");
+                    break;
                 case "Search":
                     string found = Search(d);
                     if (found != null) Console.WriteLine(found);
@@ -31,14 +37,15 @@ namespace SistemasOperacionais
                     else Console.WriteLine("Key does not exist");
                     break;
                 case "Remove":
-                    if (Remove(d) Console.WriteLine("Successfully removed");
+                    if (Remove(d)) Console.WriteLine("Successfully removed");
                     else Console.WriteLine("Key does not exist");
                     break;
             }
         }
+        //insere o valor "key" e o valor "value" do dado inserido no arquivo, separando os dois valores com dois pontos para poder usar a função Split() quando pesquizar ou atualizar
         static string Search(Data d)
         {
-            StreamReader file = new StreamReader(path, true);
+            StreamReader file = new StreamReader(path);
 
             if (file.EndOfStream)
             {
@@ -79,6 +86,7 @@ namespace SistemasOperacionais
 
             return true;
         }
+        //utiliza um arquivo temporario para armazenar as informações antigas do dado e substitui as informações que precisam ser atualizadas
         static bool Update(Data d)
         {
             string path2 = temporaryFile + path;
@@ -105,21 +113,25 @@ namespace SistemasOperacionais
             tempFile.Close();
             if (updated)
             {
+                //substitui as informações antigas pelas novas
                 File.Delete(path);
                 File.Move(path2, path);
             }
             else
             {
+                //se não emcontrar "key" do dado inserido, deleta o arquivo temporario
                 File.Delete(path2);
+
             }
             return updated;
         }
+        //utiliza um arquivo temporario que tem as informações antigas e compara o "key" do dado inserido com as informações do arquivo original para deletar
         static bool Remove(Data d)
         {
             string path2 = temporaryFile + path;
             bool removed = false;
             StreamWriter tempFile = new StreamWriter(path2, true);
-            StreamReader file = new StreamReader(file);
+            StreamReader file = new StreamReader(path);
             if (file == null || file.EndOfStream || tempFile == null)
             {
                 return false;
@@ -136,11 +148,13 @@ namespace SistemasOperacionais
             tempFile.Close();
             if (removed)
             {
+                //se encontrar o dado inserido substitui o arquivo pelo temporario, que não possui dados removidos
                 File.Delete(path);
-                File.Move(temporaryFile, file);
+                File.Move(temporaryFile, path);
             }
             else
             {
+                //se não emcontrar "key" do dado inserido, deleta o arquivo temporario
                 File.Delete(temporaryFile);
             }
             return removed;
